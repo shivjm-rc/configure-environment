0737a735397416fc5af221eb87ed06a76feb8bda
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2021-10-24 02:27:32 +0530

    feat: add main configuration

---
+++ 02-configure.ps1
@@ -0,0 +1,52 @@
+param(
+    [Parameter(Mandatory = $True)]
+    [String]$AppsSource,
+    [Parameter(Mandatory = $True)]
+    [String]$UserProfileSource,
+    [Parameter(Mandatory = $True)]
+    [String]$MediaSource,
+    [Parameter(Mandatory = $False)]
+    [String]$AllTheIconsCommit
+)
+
+$ErrorActionPreference = "Stop"
+
+Import-Module './env.psm1'
+Import-Module './fonts.psm1'
+
+. './python.ps1'
+
+Write-Output "Copying .gitconfig…"
+$script:GitConfigSource = $UserProfileSource + '\.gitconfig'
+Copy-Item -Force -Path $script:GitConfigSource -Destination $env:USERPROFILE\.gitconfig -Recurse
+
+Write-Output "Setting both ``GNUPG_HOME`` and ``GNUPGHOME``…"
+$script:GpgHome = $MediaSource + '\My\GPG'
+SetMachineEnv -Name "GNUPG_HOME" -Value $script:GpgHome
+SetMachineEnv -Name "GNUPGHOME" -Value $script:GpgHome
+
+Write-Output "Copying Emacs config…"
+$script:EmacsDirectoriesToExclude = @('straight', '.cache', 'eln-cache', 'elpa', 'elpy', 'image-dired', 'server', 'snippets', '.lsp-session*')
+$script:EmacsSourceDirectory = "$UserProfileSource\AppData\Roaming\.emacs.d"
+$script:EmacsDestinationDirectory = "$env:APPDATA\.emacs.d"
+New-Item -Path $script:EmacsDestinationDirectory -Force -ItemType Directory
+Copy-Item -Path $script:EmacsSourceDirectory\* -Recurse -Exclude $script:EmacsDirectoriesToExclude -Destination $script:EmacsDestinationDirectory -Force
+
+Write-Output "Installing all-the-icons fonts…"
+$script:AllTheIconsCommit = ($AllTheIconsCommit -eq "" ? "master" : $AllTheIconsCommit)
+$script:AllTheIconsUrl = "https://github.com/domtronn/all-the-icons.el/archive/$script:AllTheIconsCommit.zip"
+Write-Output "Downloading $script:AllTheIconsUrl…"
+Invoke-WebRequest -OutFile $env:TEMP\all-the-icons.zip -Uri $script:AllTheIconsUrl
+Expand-Archive -Force -LiteralPath $env:TEMP\all-the-icons.zip -DestinationPath $env:TEMP\all-the-icons
+try {
+    $script:expr = "Install-Fonts `"$env:TEMP\all-the-icons\all-the-icons.el-$($script:AllTheIconsCommit)\fonts`""
+    Start-Process pwsh -Verb runas -ArgumentList ('-c', $script:expr) -Wait -WindowStyle Hidden
+}
+finally {
+    Remove-Item -Path $env:TEMP\all-the-icons* -Recurse -Force
+}
+
+Copy-Item -Path $AppsSource\Scoop\persist -Recurse -Force -Destination $env:SCOOP
+Copy-Item -Path $AppsSource\ScoopApps\persist -Recurse -Force -Destination $env:SCOOP_GLOBAL
+
+New-Item -Path "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Value "Invoke-Expression '. `"D:\Media\src\my\powershell\profile.ps1`"'" -Force
