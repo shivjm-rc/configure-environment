880f9c39709acdfbfaedc208da98bd1e13aa4efe
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2021-10-24 02:27:21 +0530

    feat: add Python configuration

---
+++ python.ps1
@@ -0,0 +1,14 @@
+Import-Module './env.psm1'
+
+Write-Output "Importing Python registry file with sudo…"
+sudo reg import "$env:SCOOP\apps\python\current\install-pep-514.reg"
+
+Write-Output "Installing pipx…"
+python3 -m pip install --user pipx
+
+Write-Output "Adding $script:PythonScriptsDirectory to ``PATH``…"
+$script:PythonDirectory = (Get-ChildItem -path "$env:APPDATA\python") | Select-Object -first 1
+$script:PythonScriptsDirectory = "$($script:PythonDirectory)\Scripts"
+$script:ExistingPath = [Environment]::GetEnvironmentVariable("PATH", "Machine")
+$script:NewPath = $script:ExistingPath + ";" + $script:PythonScriptsDirectory
+SetMachineEnv -Name 'PATH' -Value $script:NewPath
