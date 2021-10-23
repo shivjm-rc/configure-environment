3dd73fed454336d2f751eaa07b583d60cbad068b
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2021-10-24 02:04:26 +0530

    feat: add initialization

---
+++ 00-initialize.ps1
@@ -0,0 +1,15 @@
+<#
+.Synopsis
+ Prepares the PowerShell environment.
+
+.Description
+ Prepares the PowerShell environment. Requires elevated privileges.
+#>
+
+param()
+
+$ErrorActionPreference = "Stop"
+
+if ($PSVersionTable.PSVersion -lt ((Invoke-RestMethod 'https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/metadata.json').ReleaseTag -replace '^v')) {
+    iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
+}
