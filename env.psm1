6d7406ebbdaa9ec17f2f2e339864d16f609d68d6
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2021-10-24 02:15:23 +0530

    feat: add env module

---
+++ env.psm1
@@ -0,0 +1,64 @@
+# stolen from Scoop:
+# https://github.com/lukesampson/scoop/blob/092005046454d94d141d5c68fdbdb4c4a1229ae9/lib/core.ps1#L117-L121
+<#
+ .Synopsis
+  Checks whether the current user is an administrator.
+
+ .Outputs
+  System.Boolean. IsAdmin returns `$True` if the current user is an administrator, `$False` otherwise.
+#>
+function Script:IsAdmin {
+    $admin = [security.principal.windowsbuiltinrole]::administrator
+    $id = [security.principal.windowsidentity]::getcurrent()
+    ([security.principal.windowsprincipal]($id)).isinrole($admin)
+}
+
+Export-ModuleMember -Function IsAdmin
+
+<#
+ .Synopsis
+  Sets an environment variable at the machine level.
+
+ .Description
+  Sets an environment variable at the machine level. Does nothing if the environment variable is already set to `$Value`. Attempts to acquire elevated privileges if necessary.
+
+ .Parameter Name
+  Specifies the name of the variable.
+
+ .Parameter Value
+  Specifies the value of the variable.
+
+ .Example
+
+  PS> SetMachineEnv -Name FOO -Value bar
+
+  PS> $env:FOO
+  bar
+#>
+Function Script:SetMachineEnv {
+    param(
+        [Parameter(Mandatory = $True)]
+        [string]$Name,
+        [Parameter(Mandatory = $True)]
+        [string]$Value
+    )
+
+    if ((Get-Item -Path "Env:\\$Name" -ErrorAction SilentlyContinue).Value -eq $Value) {
+        Write-Output "Environment variable $Name already set, skipping…"
+        return
+    }
+
+    $expr = "[Environment]::SetEnvironmentVariable('$Name', '$Value', 'Machine')"
+
+    if (IsAdmin) {
+        Write-Output "Setting system environment variable $Name…"
+        Invoke-Expression $expr
+    } else {
+        Write-Output "Setting system environment variable $Name in elevated process…"
+        Start-Process pwsh -Verb runas -ArgumentList ('-c', $expr) -Wait -WindowStyle Hidden
+    }
+
+    Set-Item -Path "Env:\\$Name" -Value $Value
+}
+
+Export-ModuleMember -Function SetMachineEnv
