97ef3db441a270f5d0493a0e1ea468378b8157e3
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2021-11-05 03:52:58 +0530

    feat(env): add helper variables

--- env.psm1
+++ env.psm1
@@ -75,3 +75,6 @@ function Get-ScriptDirectory {
     $Invocation = (Get-Variable MyInvocation -Scope 1).Value
     Split-Path $Invocation.MyCommand.Path
 }
+
+$Global:ConfigDirectory = (Get-ScriptDirectory)
+$Global:PackagesDirectory = "$Global:ConfigDirectory\\package-lists"
