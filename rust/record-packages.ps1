7eece92b8bf6868e1a365c74320637186163fce2
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2022-02-26 14:27:07 +0530

    feat: add rust/record-packages.ps1

---
+++ rust/record-packages.ps1
@@ -0,0 +1,57 @@
+<#
+.SYNOPSIS
+ Records latest versions of crates.io packages in the given file.
+#>
+param(
+    [Parameter(Mandatory = $True, Position = 1)]
+    [string]
+    $filename
+)
+
+$headerMarker = "Package*"
+
+$cargoUpdateOutput = (cargo install-update -l) -split "`n"
+
+$pastHeader = $false
+
+$packages = @{}
+
+foreach ($line in $cargoUpdateOutput) {
+    if (!$pastHeader) {
+        if ($line -like $headerMarker) {
+            $pastHeader = $true
+        }
+
+        continue
+    }
+
+    $parts = $line -split "\s+"
+
+    if ($parts.Length -lt 4) {
+        continue
+    }
+
+    $name = $parts[0]
+    $version = $parts[1] -replace "v",""
+
+    Write-Debug "Package: $name $version"
+
+    $packages[$name] = $version
+}
+
+$existing = ConvertFrom-Json (Get-Content $filename -Raw)
+
+$new = @()
+
+foreach ($package in $existing) {
+    if ($null -ne $package.version) {
+        $package.version = $packages[$package.package]
+    } else {
+        Write-Debug "Skipping non-versioned package $($package.package)"
+    }
+
+    $new += $package
+}
+
+Set-Content $filename (ConvertTo-Json $new)
+prettier -w $filename
