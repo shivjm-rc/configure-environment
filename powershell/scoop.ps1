4789c2f012ddff2e424ef507eef80d154615f28a
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2023-03-12 16:27:09 +0530

    feat(scoop): improve logic and logging

--- powershell/scoop.ps1
+++ powershell/scoop.ps1
@@ -3,29 +3,24 @@ Import-Module "./env.psm1"
 SetMachineEnv -Name 'SCOOP' -Value 'C:\App\Scoop'
 SetMachineEnv -Name 'SCOOPGLOBAL' -Value 'C:\App\ScoopApps'
 
-if ((Get-Command scoop -ErrorAction SilentlyContinue) -eq $null) {
+if ($null -eq (Get-Command scoop -ErrorAction SilentlyContinue)) {
     Set-ExecutionPolicy RemoteSigned -scope Process
-    iwr get.scoop.sh | iex
+    Invoke-WebRequest get.scoop.sh | Invoke-Expression
+} else {
+    Write-Verbose "Scoop already installed, skipping…"
 }
-else {
-    Write-Output "Scoop already installed, skipping…"
+
+if ($null -eq (scoop list aria2 6>$null | select-string aria2)) {
+    scoop install aria2
+    scoop config aria2-warning-enabled false
 }
 
-scoop config aria2-warning-enabled false
+# $script:ScoopApps = (Get-Content "$Global:PackagesDirectory\\scoop")
+# Write-Output "Installing $($script:ScoopApps.Count) Scoop apps…"
+# scoop install @script:ScoopApps
+# scoop uninstall vcredist2019    # remove leftover installer
+# # TODO install TeraCopy
 
-Write-Output "Installing Git to add buckets…"
-scoop install git
-
-Write-Output "Installing aria2 for faster downloads…"
-scoop bucket add extras
-scoop install aria2
-
-$script:ScoopApps = (Get-Content "$Global:PackagesDirectory\\scoop")
-Write-Output "Installing $($script:ScoopApps.Count) Scoop apps…"
-scoop install @script:ScoopApps
-scoop uninstall vcredist2019    # remove leftover installer
-# TODO install TeraCopy
-
-if ($LASTEXITCODE -ne 0) {
-    exit 1
-}
+# if ($LASTEXITCODE -ne 0) {
+#     exit 1
+# }
