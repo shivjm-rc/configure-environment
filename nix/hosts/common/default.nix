10bff78e2dd7182c7ee1c5f93a94fae7e22561ec
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2023-05-22 21:27:04 +0530

    feat(nix): enable Docker

--- nix/hosts/common/default.nix
+++ nix/hosts/common/default.nix
@@ -78,6 +78,8 @@
     passwordAuthentication = false;
   };
 
+  virtualisation.docker.enable = true;
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "22.11";
 }
