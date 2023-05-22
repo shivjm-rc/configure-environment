e53a2d880d9e213ed0ae6fa4d0bf83f69a867eba
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2023-05-22 22:00:58 +0530

    fix(nix): use correct path to home-manager configuration in flake.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -73,13 +73,11 @@
       # Standalone home-manager configuration entrypoint
       # Available through 'home-manager --flake .#your-username@your-hostname'
       homeConfigurations = {
-        # FIXME replace with your username@hostname
         "a@A-PC" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
           extraSpecialArgs = { inherit inputs outputs; };
           modules = [
-            # > Our main home-manager configuration file <
-            ./home-manager/home.nix
+            ./home-manager/a
           ];
         };
       };
