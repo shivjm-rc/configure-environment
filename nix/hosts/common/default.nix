484c93cf57d394854c8b5c29f2c2446c2c0759ed
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2023-05-23 17:23:12 +0530

    feat(nix): use home-manager from unstable nixpkgs

--- nix/hosts/common/default.nix
+++ nix/hosts/common/default.nix
@@ -34,12 +34,9 @@
       # You can also add overlays exported from other flakes:
       # neovim-nightly-overlay.overlays.default
 
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
+      (final: prev: {
+        home-manager = pkgs.unstable.home-manager;
+      })
     ];
     # Configure your nixpkgs instance
     config = {
