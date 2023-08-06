c55f347aee9bd57c573b463d6ac3e2912afec94b
Shiv Jha-Mathur <shiv@shivjm.in>
2023-08-07 02:32:05 +0530

    feat(nix): improve logging in rclone service

--- nix/home-manager/a/rclone.nix
+++ nix/home-manager/a/rclone.nix
@@ -4,6 +4,7 @@
 { pkgs
 , targetDir ? "%h/mnt/%i"
 , configFile ? "%h/.config/rclone/rclone.conf"
+, logFile ? "%h/.log/rclone-%i"
 , ...
 }: {
   systemd.user.services = {
@@ -20,11 +21,13 @@
         Environment =
           [ "PATH=/run/wrappers/bin/:$PATH" "RCLONE_CONFIG=${configFile}" ];
         RestartSec = "5s";
-        ExecStartPre = "/run/current-system/sw/bin/mkdir -p ${targetDir}";
+        ExecStartPre = "/bin/mkdir -p ${targetDir}";
         ExecStart =
-          "${pkgs.rclone}/bin/rclone mount --config ${configFile} %i: ${targetDir} --vfs-cache-mode full --log-file %h/tmp/rclone-%i.log --allow-other --umask 022";
+          "${pkgs.rclone}/bin/rclone mount --config ${configFile} %i: ${targetDir} --vfs-cache-mode full --log-file ${logFile}.log --allow-other --umask 022";
         Restart = "on-failure";
         ExecStop = "/bin/fusermount -u ${targetDir}";
+        StandardOutput = "append:${logFile}-out.log";
+        StandardError = "append:${logFile}-err.log";
       };
 
       Install = { WantedBy = [ "default.target" ]; };
